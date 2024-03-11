eec6167c26a28cea69090c493ddbb2c81d3f1a5e
Adriane Hanson <ahansonuga@gmail.com>
2024-03-11 12:49:52 -0400

    Simplify test_check_date
    Convert df to list with function and have column list in a class variable.

--- tests/test_check_dates.py
+++ tests/test_check_dates.py
@@ -1,5 +1,8 @@
 """
-Tests for the function check_dates, which finds dates to review for deletion that have expired
+Tests for the function check_dates(), which finds dates to review for deletion that have expired.
+
+For easier testing, the dataframe with inventory data is made within the function using pandas.
+In production, it is made by reading an Excel spreadsheet using read_inventory().
 """
 import unittest
 from hub_audit import check_dates
@@ -8,122 +11,103 @@ from numpy import NaN
 from pandas import DataFrame
 
 
+def df_to_list(df):
+    """Fill blanks with the string 'nan' and convert each row in a dataframe to a list"""
+    df = df.fillna('nan')
+    df_list = [df.columns.tolist()] + df.values.tolist()
+    return df_list
+
+
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Variable used in all the tests."""
+        self.columns = ['Share (required)', 'Folder Name (required if not share)', 'Use Policy Category (required)',
+                        'Person Responsible (required)', 'Date to review for deletion (required)',
+                        'Additional information (optional)', 'Deleted (date) (optional)', 'Audit_Result']
 
     def test_combination(self):
         """Test for an inventory where the date formatting is mixed"""
-        # Makes a dataframe for function input, which would usually be made by read_inventory(), and runs the function.
-        column_names = ['Share (required)', 'Folder Name (required if not share)', 'Use Policy Category (required)',
-                        'Person Responsible (required)', 'Date to review for deletion (required)',
-                        'Additional information (optional)', 'Deleted (date) (optional)', 'Audit_Result']
-        df = DataFrame([['Share_A', 'FolderA1', 'Backlog', 'June', 'Permanent', NaN, NaN, NaN],
-                        ['Share_A', 'FolderA2', 'Backlog', 'June', datetime(2021, 1, 1, 0, 0), NaN, NaN, NaN],
-                        ['Share_B', 'FolderB', 'Backlog', 'June', '6 months', NaN, NaN, NaN],
-                        ['Share_C', 'FolderC1', 'Backlog', 'June', datetime(3031, 12, 14, 0, 0), NaN, NaN, NaN],
-                        ['Share_C', 'FolderC2', 'Backlog', 'June', 'permanent', NaN, NaN, NaN]],
-                       columns=column_names)
-        inventory_df = check_dates(df)
+        # Make a dataframe with Hub inventory data and run the function being tested.
+        rows = [['Share_A', 'A1', 'Backlog', 'June', 'Permanent', NaN, NaN, NaN],
+                ['Share_A', 'A2', 'Backlog', 'June', datetime(2021, 1, 1, 0, 0), NaN, NaN, NaN],
+                ['Share_B', 'B', 'Backlog', 'June', '6 months', NaN, NaN, NaN],
+                ['Share_C', 'FolderC1', 'Backlog', 'June', datetime(3031, 12, 14, 0, 0), NaN, NaN, NaN],
+                ['Share_C', 'FolderC2', 'Backlog', 'June', 'permanent', NaN, NaN, NaN]]
+        inventory_df = check_dates(DataFrame(rows, columns=self.columns))
 
-        # Converts the function result (dataframe) into a list, with blanks replaced by empty strings,
-        # and compares it to the expected result.
-        result = inventory_df.fillna('')
-        result = [result.columns.tolist()] + result.values.tolist()
-        expected = [column_names,
-                    ['Share_A', 'FolderA1', 'Backlog', 'June', 'Permanent', '', '', ''],
-                    ['Share_A', 'FolderA2', 'Backlog', 'June', datetime(2021, 1, 1, 0, 0), '', '', 'Date expired'],
-                    ['Share_B', 'FolderB', 'Backlog', 'June', '6 months', '', '', 'Check date'],
-                    ['Share_C', 'FolderC1', 'Backlog', 'June', datetime(3031, 12, 14, 0, 0), '', '', ''],
-                    ['Share_C', 'FolderC2', 'Backlog', 'June', 'permanent', '', '', '']]
+        # Tests if the resulting dataframe has the expected data.
+        result = df_to_list(inventory_df)
+        expected = [self.columns,
+                    ['Share_A', 'A1', 'Backlog', 'June', 'Permanent', 'nan', 'nan', 'nan'],
+                    ['Share_A', 'A2', 'Backlog', 'June', datetime(2021, 1, 1, 0, 0), 'nan', 'nan', 'Date expired'],
+                    ['Share_B', 'B', 'Backlog', 'June', '6 months', 'nan', 'nan', 'Check date'],
+                    ['Share_C', 'FolderC1', 'Backlog', 'June', datetime(3031, 12, 14, 0, 0), 'nan', 'nan', 'nan'],
+                    ['Share_C', 'FolderC2', 'Backlog', 'June', 'permanent', 'nan', 'nan', 'nan']]
         self.assertEqual(result, expected, "Problem with test for combined date formats")
 
     def test_dates_expired(self):
         """Test for an inventory where the dates are formatted as a date and the dates are in the past (expired)"""
+        # Make a dataframe with Hub inventory data and run the function being tested.
+        rows = [['Share_A', 'A1', 'Backlog', 'June', datetime(2024, 2, 29, 0, 0), NaN, NaN, NaN],
+                ['Share_A', 'A2', 'Backlog', 'June', datetime(2024, 2, 1, 0, 0), NaN, NaN, NaN],
+                ['Share_B', 'B', 'Backlog', 'June', datetime(2022, 10, 15, 0, 0), NaN, NaN, NaN]]
+        inventory_df = check_dates(DataFrame(rows, columns=self.columns))
 
-        # Makes a dataframe for function input, which would usually be made by read_inventory(), and runs the function.
-        column_names = ['Share (required)', 'Folder Name (required if not share)', 'Use Policy Category (required)',
-                        'Person Responsible (required)', 'Date to review for deletion (required)',
-                        'Additional information (optional)', 'Deleted (date) (optional)', 'Audit_Result']
-        df = DataFrame([['Share_A', 'FolderA1', 'Backlog', 'June', datetime(2024, 2, 29, 0, 0), NaN, NaN, NaN],
-                        ['Share_A', 'FolderA2', 'Backlog', 'June', datetime(2024, 2, 1, 0, 0), NaN, NaN, NaN],
-                        ['Share_B', 'FolderB', 'Backlog', 'June', datetime(2022, 10, 15, 0, 0), NaN, NaN, NaN]],
-                       columns=column_names)
-        inventory_df = check_dates(df)
-
-        # Converts the function result (dataframe) into a list, with blanks replaced by empty strings,
-        # and compares it to the expected result.
-        result = inventory_df.fillna('')
-        result = [result.columns.tolist()] + result.values.tolist()
-        expected = [column_names,
-                    ['Share_A', 'FolderA1', 'Backlog', 'June', datetime(2024, 2, 29, 0, 0), '', '', 'Date expired'],
-                    ['Share_A', 'FolderA2', 'Backlog', 'June', datetime(2024, 2, 1, 0, 0), '', '', 'Date expired'],
-                    ['Share_B', 'FolderB', 'Backlog', 'June', datetime(2022, 10, 15, 0, 0), '', '', 'Date expired']]
+        # Tests if the resulting dataframe has the expected data.
+        result = df_to_list(inventory_df)
+        expected = [self.columns,
+                    ['Share_A', 'A1', 'Backlog', 'June', datetime(2024, 2, 29, 0, 0), 'nan', 'nan', 'Date expired'],
+                    ['Share_A', 'A2', 'Backlog', 'June', datetime(2024, 2, 1, 0, 0), 'nan', 'nan', 'Date expired'],
+                    ['Share_B', 'B', 'Backlog', 'June', datetime(2022, 10, 15, 0, 0), 'nan', 'nan', 'Date expired']]
         self.assertEqual(result, expected, "Problem with test for dates, expired")
 
     def test_dates_future(self):
         """Test for an inventory where the dates are formatted as a date and the dates are in the future"""
+        # Make a dataframe with Hub inventory data and run the function being tested.
+        rows = [['Share_A', 'A1', 'Backlog', 'June', datetime(2122, 10, 15, 0, 0), NaN, NaN, NaN],
+                ['Share_A', 'A2', 'Backlog', 'June', datetime(2222, 10, 15, 0, 0), NaN, NaN, NaN],
+                ['Share_B', 'B', 'Backlog', 'June', datetime(2322, 10, 15, 0, 0), NaN, NaN, NaN]]
+        inventory_df = check_dates(DataFrame(rows, columns=self.columns))
 
-        # Makes a dataframe for function input, which would usually be made by read_inventory(), and runs the function.
-        column_names = ['Share (required)', 'Folder Name (required if not share)', 'Use Policy Category (required)',
-                        'Person Responsible (required)', 'Date to review for deletion (required)',
-                        'Additional information (optional)', 'Deleted (date) (optional)', 'Audit_Result']
-        df = DataFrame([['Share_A', 'FolderA1', 'Backlog', 'June', datetime(2122, 10, 15, 0, 0), NaN, NaN, NaN],
-                        ['Share_A', 'FolderA2', 'Backlog', 'June', datetime(2222, 10, 15, 0, 0), NaN, NaN, NaN],
-                        ['Share_B', 'FolderB', 'Backlog', 'June', datetime(2322, 10, 15, 0, 0), NaN, NaN, NaN]],
-                       columns=column_names)
-        inventory_df = check_dates(df)
-
-        # Converts the function result (dataframe) into a list, with blanks replaced by empty strings,
-        # and compares it to the expected result.
-        result = inventory_df.fillna('')
-        result = [result.columns.tolist()] + result.values.tolist()
-        expected = [column_names,
-                    ['Share_A', 'FolderA1', 'Backlog', 'June', datetime(2122, 10, 15, 0, 0), '', '', ''],
-                    ['Share_A', 'FolderA2', 'Backlog', 'June', datetime(2222, 10, 15, 0, 0), '', '', ''],
-                    ['Share_B', 'FolderB', 'Backlog', 'June', datetime(2322, 10, 15, 0, 0), '', '', '']]
+        # Tests if the resulting dataframe has the expected data.
+        result = df_to_list(inventory_df)
+        expected = [self.columns,
+                    ['Share_A', 'A1', 'Backlog', 'June', datetime(2122, 10, 15, 0, 0), 'nan', 'nan', 'nan'],
+                    ['Share_A', 'A2', 'Backlog', 'June', datetime(2222, 10, 15, 0, 0), 'nan', 'nan', 'nan'],
+                    ['Share_B', 'B', 'Backlog', 'June', datetime(2322, 10, 15, 0, 0), 'nan', 'nan', 'nan']]
         self.assertEqual(result, expected, "Problem with test for dates, future")
 
     def test_strings_not_permanent(self):
         """Test for an inventory where the dates are a string but not 'permanent' or 'Permanent'"""
-        column_names = ['Share (required)', 'Folder Name (required if not share)', 'Use Policy Category (required)',
-                        'Person Responsible (required)', 'Date to review for deletion (required)',
-                        'Additional information (optional)', 'Deleted (date) (optional)', 'Audit_Result']
-        df = DataFrame([['Share_A', 'FolderA1', 'Backlog', 'June', '6 months', NaN, NaN, NaN],
-                        ['Share_A', 'FolderA2', 'Backlog', 'June', 'year', NaN, NaN, NaN],
-                        ['Share_B', 'FolderB', 'Backlog', 'June', 'In folder title', NaN, NaN, NaN]],
-                       columns=column_names)
-        inventory_df = check_dates(df)
+        # Make a dataframe with Hub inventory data and run the function being tested.
+        rows = [['Share_A', 'A1', 'Backlog', 'June', '6 months', NaN, NaN, NaN],
+                ['Share_A', 'A2', 'Backlog', 'June', 'year', NaN, NaN, NaN],
+                ['Share_B', 'B', 'Backlog', 'June', 'In folder title', NaN, NaN, NaN]]
+        inventory_df = check_dates(DataFrame(rows, columns=self.columns))
 
-        # Converts the function result (dataframe) into a list, with blanks replaced by empty strings,
-        # and compares it to the expected result.
-        result = inventory_df.fillna('')
-        result = [result.columns.tolist()] + result.values.tolist()
-        expected = [column_names,
-                    ['Share_A', 'FolderA1', 'Backlog', 'June', '6 months', '', '', 'Check date'],
-                    ['Share_A', 'FolderA2', 'Backlog', 'June', 'year', '', '', 'Check date'],
-                    ['Share_B', 'FolderB', 'Backlog', 'June', 'In folder title', '', '', 'Check date']]
+        # Tests if the resulting dataframe has the expected data.
+        result = df_to_list(inventory_df)
+        expected = [self.columns,
+                    ['Share_A', 'A1', 'Backlog', 'June', '6 months', 'nan', 'nan', 'Check date'],
+                    ['Share_A', 'A2', 'Backlog', 'June', 'year', 'nan', 'nan', 'Check date'],
+                    ['Share_B', 'B', 'Backlog', 'June', 'In folder title', 'nan', 'nan', 'Check date']]
         self.assertEqual(result, expected, "Problem with test for strings, not permanent")
 
     def test_strings_permanent(self):
         """Test for an inventory where the dates are either 'permanent' or 'Permanent'"""
+        # Make a dataframe with Hub inventory data and run the function being tested.
+        rows = [['Share_A', 'A1', 'Backlog', 'June', 'Permanent', NaN, NaN, NaN],
+                ['Share_A', 'A2', 'Backlog', 'June', 'Permanent', NaN, NaN, NaN],
+                ['Share_B', 'B', 'Backlog', 'June', 'permanent', NaN, NaN, NaN]]
+        inventory_df = check_dates(DataFrame(rows, columns=self.columns))
 
-        # Makes a dataframe for function input, which would usually be made by read_inventory(), and runs the function.
-        column_names = ['Share (required)', 'Folder Name (required if not share)', 'Use Policy Category (required)',
-                        'Person Responsible (required)', 'Date to review for deletion (required)',
-                        'Additional information (optional)', 'Deleted (date) (optional)', 'Audit_Result']
-        df = DataFrame([['Share_A', 'FolderA1', 'Backlog', 'June', 'Permanent', NaN, NaN, NaN],
-                        ['Share_A', 'FolderA2', 'Backlog', 'June', 'Permanent', NaN, NaN, NaN],
-                        ['Share_B', 'FolderB', 'Backlog', 'June', 'permanent', NaN, NaN, NaN]],
-                       columns=column_names)
-        inventory_df = check_dates(df)
-
-        # Converts the function result (dataframe) into a list, with blanks replaced by empty strings,
-        # and compares it to the expected result.
-        result = inventory_df.fillna('')
-        result = [result.columns.tolist()] + result.values.tolist()
-        expected = [column_names,
-                    ['Share_A', 'FolderA1', 'Backlog', 'June', 'Permanent', '', '', ''],
-                    ['Share_A', 'FolderA2', 'Backlog', 'June', 'Permanent', '', '', ''],
-                    ['Share_B', 'FolderB', 'Backlog', 'June', 'permanent', '', '', '']]
+        # Tests if the resulting dataframe has the expected data.
+        result = df_to_list(inventory_df)
+        expected = [self.columns,
+                    ['Share_A', 'A1', 'Backlog', 'June', 'Permanent', 'nan', 'nan', 'nan'],
+                    ['Share_A', 'A2', 'Backlog', 'June', 'Permanent', 'nan', 'nan', 'nan'],
+                    ['Share_B', 'B', 'Backlog', 'June', 'permanent', 'nan', 'nan', 'nan']]
         self.assertEqual(result, expected, "Problem with test for strings, permanent")
 
 
