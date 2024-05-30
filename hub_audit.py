e18df68440da029b25c10fe52949e71816564abb
Adriane Hanson <ahansonuga@gmail.com>
2024-05-30 10:37:52 -0400

    Test if dir before iterate to second level

--- hub_audit.py
+++ hub_audit.py
@@ -112,20 +112,24 @@ def check_inventory(df, df_shares):
         # which is a pipe-separated string in df_shares.
         elif share.pattern == 'second':
             for item in os.listdir(share.path):
-                for second_item in os.listdir(os.path.join(share.path, item)):
-                    if item in share.folders.split('|'):
-                        # In born-digital folders, go to the third (collection) level in backlogged and closed:
-                        if item.lower() == 'born-digital' and second_item in ('backlogged', 'closed'):
-                            for third_item in os.listdir(os.path.join(share.path, item, second_item)):
+                # Continue navigation if item is a directory and stop if it is a file.
+                if os.path.isdir(os.path.join(share.path, item)):
+                    for second_item in os.listdir(os.path.join(share.path, item)):
+                        if item in share.folders.split('|'):
+                            # In born-digital folders, go to the third (collection) level in backlogged and closed:
+                            if item.lower() == 'born-digital' and second_item in ('backlogged', 'closed'):
+                                for third_item in os.listdir(os.path.join(share.path, item, second_item)):
+                                    share_inventory['Share'].append(share.name)
+                                    share_inventory['Folder'].append(f'{item}\\{second_item}\\{third_item}')
+                            else:
                                 share_inventory['Share'].append(share.name)
-                                share_inventory['Folder'].append(f'{item}\\{second_item}\\{third_item}')
+                                share_inventory['Folder'].append(f'{item}\\{second_item}')
                         else:
                             share_inventory['Share'].append(share.name)
-                            share_inventory['Folder'].append(f'{item}\\{second_item}')
-                    else:
-                        share_inventory['Share'].append(share.name)
-                        share_inventory['Folder'].append(item)
-
+                            share_inventory['Folder'].append(item)
+                else:
+                    share_inventory['Share'].append(share.name)
+                    share_inventory['Folder'].append(item)
         # Catch shares with unexpected patterns.
         else:
             print(f'Error: config has an unexpected pattern')
